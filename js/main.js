76ca283d0a4f05d5036df364303f01b633ad731a
Stephanie G Malek <stephanie.g.malek@gmail.com>
2020-01-17 11:30:07 -0500

    inital modal working and font icons added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+//to make top nav responsive//
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+//skills chart //
 setTimeout(function start (){
 
   $('.bar').each(function(i){
@@ -21,3 +31,54 @@ $('.count').each(function () {
 });
 
 }, 500)
+// Work lightbox / Modal Gallery //
+//$(document).ready(function(){
+
+  //Modal Text / details
+  var modalText = {
+    rogue: {
+      title: 'Rogue Pickings',
+      tag: 'Resturant Website',
+      detail:'Fictional company website. Built using only HTML and CSS.'
+    }
+  };
+//Open the modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+//Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+//Next/previous Controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image Controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+//})
